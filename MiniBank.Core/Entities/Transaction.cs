ef0f387d3f1015e4828ff508092c8ea0586dab59
Valery Shevchenko <ar4er.valera@gmail.com>
2022-03-16 12:00:58 +0300

    feat: entities

---
+++ MiniBank.Core/Entities/Transaction.cs
@@ -0,0 +1,19 @@
+﻿namespace MiniBank.Core.Entities;
+
+public class Transaction
+{
+    public Transaction(double amount, string currency, Guid fromAccountId, Guid toAccountId)
+    {
+        Id = Guid.NewGuid();
+        Amount = amount;
+        Currency = currency;
+        FromAccountId = fromAccountId;
+        ToAccountId = toAccountId;
+    }
+
+    public Guid Id { get; set; }
+    public double Amount { get; set; }
+    public string Currency { get; set; }
+    public Guid FromAccountId { get; set; }
+    public Guid ToAccountId { get; set; }
+}
---
+++ MiniBank.Core/Entities/User.cs
@@ -0,0 +1,15 @@
+﻿namespace MiniBank.Core.Entities;
+
+public class User
+{
+    public User(string login, string email)
+    {
+        Id = Guid.NewGuid();
+        Login = login;
+        Email = email;
+    }
+
+    public Guid Id { get; set; }
+    public string Login { get; set; }
+    public string Email { get; set; }
+}
